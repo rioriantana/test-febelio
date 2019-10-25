f484220cb359c613e9442a1330943e6651f3102e
Rio Riantana <rioriantana@yahoo.com>
2019-10-25 09:47:55 +0700

    final

--- app/config/database.js
+++ app/config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/fabelio'
+    //url: 'mongodb://localhost:27017/fabelio'
+    url: 'mongodb+srv://rioriantana:fabelio123@cluster0-yg6jd.mongodb.net/fabelio?retryWrites=true&w=majority'
 }
